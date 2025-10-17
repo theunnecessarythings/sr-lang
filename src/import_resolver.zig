48c3aa210aaeee83a9ce55c2d32ea00ac28666cf
Sreeraj Ramachandran <sreerajr000@gmail.com>
2025-10-17 01:31:22 -0500

    Build package metadata for import resolution

--- src/import_resolver.zig
+++ src/import_resolver.zig
@@ -3,5 +3,7 @@ const module_graph = @import("module_graph.zig");
 pub const ModuleEntry = module_graph.ModuleEntry;
 pub const LoadMode = module_graph.LoadMode;
 pub const ModuleGraph = module_graph.ModuleGraph;
+pub const PackageInfo = module_graph.PackageInfo;
+pub const PackageId = module_graph.PackageId;
 
 pub const ImportResolver = module_graph.ModuleGraph;
